4b8b7f060b0e1fbf8b560b3944267f72bc961693
AgeevDmitryMinsk <ageev.dmitry@outlook.com>
2022-03-10 16:53:11 +0300

    changed avatar HW1 + deploy

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -4,8 +4,8 @@ import Message from "./Message";
 import photoDimich from "../../dimich_fasebook.jpg"
 
 const messageData = {
-    avatar: "https://scontent.fmsq2-1.fna.fbcdn.net/v/t1.18169-9/10451136_627830624018135_2043333501645767488_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=fxH7v3V-IbEAX98L-3G&_nc_ht=scontent.fmsq2-1.fna&oh=00_AT9y3cqTQfjkJDXy-AkLrEpoS_5BQY-amBq4r_mdTvFzrQ&oe=62231D9F",
-    name: "Dimich",
+    avatar: "https://phonoteka.org/uploads/posts/2021-05/1622021938_9-phonoteka_org-p-pop-arti-krasivo-11.jpg",
+    name: "Marilyn Monroe",
     message: `Прекрасного воскресного дня! Have a beautiful Sunday afternoon! Ich wünsche Ihnen einen schönen Sonntagnachmittag`,
     time: new Date().toLocaleTimeString(),
 }
